819be109e4d98181e90381d96e443e6f63e3f21d
Justin <nalta45@gmail.com>
2018-08-28 10:28:30 -0400

    removes dlbp functions

--- hypergraph_embedding/data_util.py
+++ hypergraph_embedding/data_util.py
@@ -9,19 +9,6 @@ from collections import namedtuple
 Paper = namedtuple("Paper", ['title', 'authors'])
 
 
-def ParseDblpXml(xml_source):
-  """
-    Parses the DBLP xml dump into a co-authorship hypergraph.
-    Expects data from dblp.uni-trier.de
-    Parsing info: https://dblp.uni-trier.de/faq/How+to+parse+dblp+xml.html
-  """
-  utf8_parser = etree.XMLParser(encoding='utf-8')
-  tree = etree.parse(xml_source, parser=utf8_parser)
-  result = Hypergraph()
-  author_to_idx = {}
-  paper_to_idx = {}
-
-
 def ParseAMiner(aminer_source):
   """
       Parses data in AMiner's format.
