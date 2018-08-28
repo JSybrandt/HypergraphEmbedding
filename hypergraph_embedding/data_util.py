42deb0dd5035a0475ad35902f3251c3ea60c4e27
Justin <nalta45@gmail.com>
2018-08-27 20:48:23 -0400

    adds data util

---
+++ hypergraph_embedding/data_util.py
@@ -0,0 +1,10 @@
+# This file is responsible for preparing experimental data
+
+
+def ParseDblpXml(xml_path):
+  """
+    Parses the DBLP xml dump into a co-authorship hypergraph.
+    Expects data from dblp.uni-trier.de
+    Parsing info: https://dblp.uni-trier.de/faq/How+to+parse+dblp+xml.html
+    """
+  pass
