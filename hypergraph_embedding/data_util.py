8a3c846e114a416a85be25fb465a3c756caa7598
Justin Sybrandt <nalta45@gmail.com>
2018-08-28 09:52:00 -0400

    adds aminer tests

--- hypergraph_embedding/data_util.py
+++ hypergraph_embedding/data_util.py
@@ -3,6 +3,10 @@
 from . import Hypergraph
 from .hypergraph_util import *
 from lxml import etree
+from collections import namedtuple
+
+# Used to store paper data
+Paper = namedtuple("Paper", ['title', 'authors'])
 
 
 def ParseDblpXml(xml_source):
@@ -16,3 +20,24 @@ def ParseDblpXml(xml_source):
   result = Hypergraph()
   author_to_idx = {}
   paper_to_idx = {}
+
+
+def ParseAMiner(aminer_source):
+  """
+      Parses data in AMiner's format.
+      Ignores all fields except title and authors
+      More information on this format here: https://aminer.org/aminernetwork
+
+      Input:
+        - aminer_source : a file-like object
+      Output: (yield)
+        - a list of Papers (named tuple)
+    """
+
+  last_seen_title = None
+  for line in aminer_source:
+    if line.startswith("#*"):  # paper title line
+      last_seen_title = line[2:].strip()
+    elif line.startswith("#@"):  # authors line
+      authors = line[2:].strip().split(';')
+      yield Paper(title=last_seen_title, authors=authors)
--- tests/test_data_util.py
+++ tests/test_data_util.py
@@ -88,3 +88,45 @@ class TestDataUtil(unittest.TestCase):
     expected.edge[0].nodes.append(1)
 
     self.assertEqual(actual, expected)
+
+
+class TestAMinerUtil(unittest.TestCase):
+
+  def test_small(self):
+    _input = ("#* T\n" "#@ A;B")
+    actual = [p for p in ParseAMiner(StringIO(_input))]
+    expected = [Paper(title="T", authors=["A", "B"])]
+    self.assertEqual(actual, expected)
+
+  def test_large(self):
+    "AMiner parsing needs to ignore all irrelevant fields"
+    DATA_PATH = "test_data/aminer_example.txt"
+    with open(DATA_PATH) as ifile:
+      actual = [p for p in ParseAMiner(ifile)]
+    expected = [
+        Paper(
+            "ArnetMiner: extraction and mining of academic social networks",
+            [
+                "Jie Tang",
+                "Jing Zhang",
+                "Limin Yao",
+                "Juanzi Li",
+                "Li Zhang",
+                "Zhong Su"
+            ])
+    ]
+    self.assertEqual(actual, expected)
+
+  def test_multiple(self):
+    "AMiner parsing needs to handle multiple papers properly"
+    _input = ("#* T\n" "#@ A;B\n" "#* T2\n" "#@ C;D\n")
+    actual = [p for p in ParseAMiner(StringIO(_input))]
+    expected = [
+        Paper(title="T",
+              authors=["A",
+                       "B"]),
+        Paper(title="T2",
+              authors=["C",
+                       "D"])
+    ]
+    self.assertEqual(actual, expected)
