8471e2517cee1717295d65b65821bdcf1dfbec90
Justin <nalta45@gmail.com>
2018-09-04 14:45:18 -0400

    improved results viewer for cumulative statistics

--- results_viewer.py
+++ results_viewer.py
@@ -8,6 +8,7 @@ import sys
 from textwrap import dedent
 from hypergraph_embedding import *
 from pathlib import Path
+from statistics import stdev
 
 
 def ParseArgs():
@@ -18,6 +19,16 @@ def ParseArgs():
       type=str,
       help="Path to the results proto buffer",
       nargs="+")
+  parser.add_argument(
+      "-i",
+      "--individual",
+      action="store_true",
+      help="If set, print each individual result")
+  parser.add_argument(
+      "-c",
+      "--cumulative",
+      action="store_true",
+      help="If set, group results by experiment key and print group stats.")
   return parser.parse_args()
 
 
@@ -44,12 +55,62 @@ def PrintResult(result):
           metrics=result.metrics))
 
 
+def ExperimentKey(result):
+  return "{graph} {method}:{dim}".format(
+      graph=result.hypergraph.name,
+      method=result.embedding.method_name,
+      dim=result.embedding.dim)
+
+
+def PrintCumulativeResult(key, results):
+  "Results is an iterable container of EvaluationMetric messages, all related"
+  "to the same experiment"
+
+  def meanAndStd(data):
+    return (sum(data) / len(data), stdev(data))
+
+  m_acc, s_acc = meanAndStd([r.accuracy for r in results])
+  m_pre, s_pre = meanAndStd([r.precision for r in results])
+  m_rec, s_rec = meanAndStd([r.recall for r in results])
+  m_f1, s_f1 = meanAndStd([r.f1 for r in results])
+
+  result_text = dedent(
+      """\
+  Experiment: {key}
+  Trials:     {trials}
+
+  Value     |  MEAN  |  STD   |
+  -----------------------------
+  Accuracy  | {ma:5.4f} | {sa:5.4f} |
+  Precision | {mp:5.4f} | {sp:5.4f} |
+  Recall    | {mr:5.4f} | {sr:5.4f} |
+  F1        | {mf:5.4f} | {sf:5.4f} |
+  -----------------------------
+      """)
+  print(
+      result_text.format(
+          key=key,
+          trials=len(results),
+          ma=m_acc,
+          sa=s_acc,
+          mp=m_pre,
+          sp=s_pre,
+          mr=m_rec,
+          sr=s_rec,
+          mf=m_f1,
+          sf=s_f1))
+
+
 if __name__ == "__main__":
   args = ParseArgs()
+  # Checking that either print is set, otherwise whats the point?
+  assert args.cumulative or args.individual
 
   proto_paths = [Path(proto) for proto in args.protos]
   for path in proto_paths:
     assert path.is_file()
+
+  experiment2metrics = {}
 
   for path in proto_paths:
     with path.open('rb') as proto_file:
@@ -61,4 +122,13 @@ if __name__ == "__main__":
             "[ERROR]:{} is not an Experimental Result".format(path),
             file=sys.stderr)
         exit(1)
-      PrintResult(result)
+      if args.individual:
+        PrintResult(result)
+      if args.cumulative:
+        key = ExperimentKey(result)
+        if key not in experiment2metrics:
+          experiment2metrics[key] = []
+        experiment2metrics[key].append(result.metrics)
+  if args.cumulative:
+    for key, results in experiment2metrics.items():
+      PrintCumulativeResult(key, results)
