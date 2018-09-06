8b3e878225eb8b2f02f22159b2e58767c14b2d5f
Justin <nalta45@gmail.com>
2018-09-06 18:13:23 -0400

    adds visualization

--- results_viewer.py
+++ results_viewer.py
@@ -3,14 +3,21 @@
 # This file investigates the output of ExperimentalResult
 # proto messages
 
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
 import argparse
 import sys
 from textwrap import dedent
 from hypergraph_embedding import *
 from pathlib import Path
 from statistics import stdev
+from scipy.linalg import svd
+import numpy as np
 import logging
 import logging.handlers
+from random import sample
 
 log = logging.getLogger()
 
@@ -35,6 +42,16 @@ def ParseArgs():
       "--cumulative",
       action="store_true",
       help="If set, group results by experiment key and print group stats.")
+  parser.add_argument(
+      "-p",
+      "--picture",
+      type=str,
+      help="If set, run SVD on embeddings and create a 2d plot.")
+  parser.add_argument(
+      "--picture-samples",
+      type=int,
+      help="Number of embeddings to represent in picture.",
+      default=1000)
   parser.add_argument(
       "protos",
       type=str,
@@ -130,17 +147,61 @@ def PrintCumulativeResult(key, results):
           f1_line=TableDataToLine(TableData([r.f1 for r in results]))))
 
 
+def PrintPicture(result, path, num_samples):
+  log.info("Setting up matplotlib")
+  fig = plt.figure()
+  node_ax = fig.add_subplot(121)
+  edge_ax = fig.add_subplot(122)
+
+  def project_2d_samples(idx2emb):
+    raw = np.zeros((num_samples, result.embedding.dim), dtype=np.float16)
+    for row, idx in enumerate(sample(idx2emb.keys(), min(num_samples, len(idx2emb)))):
+      raw[row, :] = idx2emb[idx].values
+    U, _, _ = svd(raw, full_matrices=0, overwrite_a=True)
+    return (U[:, 0], U[:, 1])
+
+  log.info("Projecting nodes into 2d using svd")
+  x, y = project_2d_samples(result.embedding.node)
+  node_ax.scatter(x, y)
+  node_ax.set_title("Nodes")
+
+  log.info("Projecting edges into 2d using svd")
+  x, y = project_2d_samples(result.embedding.edge)
+  edge_ax.scatter(x, y)
+  edge_ax.set_title("Edges")
+
+  log.info("Saving %s", path)
+  plt.tight_layout()
+  fig.suptitle(
+      "{} {}:{}".format(
+          result.hypergraph.name,
+          result.embedding.method_name,
+          result.embedding.dim))
+  fig.subplots_adjust(top=0.9)
+  fig.savefig(path)
+
+
 if __name__ == "__main__":
   args = ParseArgs()
   ConfigureLogger(args)
 
   log.info("Checking that either print is set, otherwise whats the point?")
-  assert args.cumulative or args.individual
+  assert args.cumulative or args.individual or args.picture
 
   log.info("Checking that each provided path exists")
   proto_paths = [Path(proto) for proto in args.protos]
   for path in proto_paths:
     assert path.is_file()
+
+  if args.picture:
+    log.info("Checking that the number of embeddings to sample is positive")
+    assert args.picture_samples > 0
+    picture_path = Path(args.picture)
+    log.info("Checking that the picture path is writable")
+    assert not picture_path.is_file()
+    assert picture_path.parent.is_dir()
+    log.info("For now, we are only going to make a picture for a single proto")
+    assert len(proto_paths) == 1
 
   experiment2metrics = {}
 
@@ -162,6 +223,8 @@ if __name__ == "__main__":
         if key not in experiment2metrics:
           experiment2metrics[key] = []
         experiment2metrics[key].append(result.metrics)
+      if args.picture:
+        PrintPicture(result, picture_path, args.picture_samples)
   if args.cumulative:
     for key, results in experiment2metrics.items():
       PrintCumulativeResult(key, results)
