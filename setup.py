c460699b2d9f3965ac52cade773e114a687827a5
JSybrandt <nalta45@gmail.com>
2019-01-07 14:01:51 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='HypergraphEmbedding',
+    version='0.1dev',
+    packages=['hypergraph_embedding',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
